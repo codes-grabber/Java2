dc35efc867dd0f454e174354c3f3825638a480d2
WORK <codes-grabber@yandex.ru>
2021-11-21 01:35:51 +0400

    1

--- Lesson2.3_Collection/src/Main.java
+++ Lesson2.3_Collection/src/Main.java
@@ -1,11 +1,14 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
+        ((ArrayList)list).ensureCapacity(1000); //увеличь размер до 1000
+        ((ArrayList)list).trimToSize(); // обрежь размер до необходимого
+
+
+
         list.add(5);
         list.add(1);
         list.add(9);
@@ -17,11 +20,14 @@ public class Main {
 
         System.out.println(list);
 
-//        list.addAll(Arrays.asList(7, 5, 8, 3, 5, 4)); //добавляем сразу несколько элтов
-//        System.out.println(list);
-//
-//        List<Integer> list2 = new ArrayList<>(Arrays.asList(7, 5, 8, 3, 5, 4)); //добавляем сразу несколько элтов
-//        System.out.println(list2);
+
+        //---------добавление---------
+
+        list.addAll(Arrays.asList(7, 5, 8, 3, 5, 4)); //добавляем сразу несколько элтов
+        System.out.println(list);
+
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(7, 5, 8, 3, 5, 4)); //добавляем сразу несколько элтов
+        System.out.println(list2);
 
         list.add(2, 78); //вставка
         System.out.println(list);
@@ -36,6 +42,36 @@ public class Main {
         list.remove((Integer) 2); //по значению
         System.out.println(list);
 
+        list.removeAll(Arrays.asList(2,9,5)); // удаляем пачку
+        System.out.println(list);
+
+        list.removeIf(x -> x < 5 && x> 50); // удаляем по условию
+        System.out.println(list);
+
+        //----------изменение-----------
+
+        list.replaceAll(x -> x % 2 == 0 ? x * 2 : x); //если икс делится на 2 то
+        System.out.println(list);
+
+        //------------сортировка----------
+        list.sort(Comparator.naturalOrder());  //Сортирует по увеличению
+        System.out.println(list);
+
+        list.sort(Comparator.reverseOrder());  //В обратном порядке
+        System.out.println(list);
+
+        list.sort(Comparator.comparingInt(x -> x % 3)); //сортирует от остатка
+
+        List<String> strlist = new ArrayList<>(Arrays.asList("Barsik", "Amur", "Yo"));
+        strlist.sort(Comparator.comparingInt(s -> s.length()));
+        System.out.println(strlist);
+
+        //---------получение
+        System.out.println(list.get(3));
+        list.set(4, 777); // заменить 4й эл-т на 777
+
+        System.out.println(list.indexOf(2)); // вернет второй эл.т
+        System.out.println(list.lastIndexOf(2)); // верней второй эл-т с конца
 
     }
 }
