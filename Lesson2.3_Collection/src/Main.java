82a6e22c7c9b6bb6efdacdd7ee1d7aefb96a03ee
WORK <codes-grabber@yandex.ru>
2021-11-21 17:38:06 +0400

    1

--- Lesson2.3_Collection/src/Main.java
+++ Lesson2.3_Collection/src/Main.java
@@ -2,6 +2,7 @@ import com.sun.org.apache.bcel.internal.generic.LineNumberGen;
 
 import java.beans.IntrospectionException;
 import java.util.*;
+import java.util.function.BiConsumer;
 import java.util.function.Predicate;
 
 public class Main {
@@ -134,6 +135,101 @@ public class Main {
         set2.add(654424);
         set2.add(4);   //не добавит эллемонт потому что такой уже есть
         System.out.println(set2); // [4, 5, 65, 654, 654424]в порядке возрастания
+
+        //------------Множества-------
+        Set<Integer> setA = new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6));
+        Set<Integer> setB = new HashSet<>(Arrays.asList(2, 4, 6, 8, 10));
+
+        System.out.println("A : " + setA);
+        System.out.println("B : " + setB);
+
+//      setA.retainAll(setB); //пересечения [2, 4, 6]
+//      System.out.println("A ^ B: " + setA);
+
+//        setA.addAll(setB); //обьединение
+//        System.out.println("A v B: " + setA);
+//
+//        setA.removeAll(setB); //пересечения с отрицанием (вычитания) [1, 3, 5]
+//        System.out.println(("A ^ -B: " + setA));
+
+        //------------Map-------
+
+        Map<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map1 = new LinkedHashMap<>();
+        Map<String, Integer> map2 = new TreeMap<>();
+
+        map.put("Barsik", 4);
+        map.put("Amur", 2);
+        map.put("Murzik", 7);
+//        map.put("Amur", 2); //Амур перезапишется последним
+        map.putIfAbsent("Amur", 9); //если отсутствует тогда вставь, иначе put просто перезапишет
+
+        map1.put("Barsik", 4);
+        map1.put("Amur", 2);
+        map1.put("Murzik", 7);
+        map1.put("Jack", 2);
+
+        map2.put("Barsik", 4);
+        map2.put("Amur", 2);
+        map2.put("Murzik", 7);
+
+        System.out.println(map);
+        System.out.println(map1);
+        System.out.println(map2);
+
+        System.out.println(map.get("Amur")); // для не найденного ключа вернет null
+
+        System.out.println(map.getOrDefault("Amurее", 0)); //если найдет то выведет, если не найдет вернет ноль
+
+        System.out.println(map.keySet()); // вытаскивает ключи [Amur, Murzik, Barsik]
+        System.out.println(map.values());  // вытащит значения [2, 7, 4]
+
+
+        System.out.println(map.containsKey("Amur")); //Содержит ли ключ если да то вернет true
+        System.out.println(map.containsValue("2")); //Содержит ли значение если да то вернет true
+        //merge позволяет пройтись по списку и преобразовать
+
+        //---------обход map--------
+        for (Map.Entry<String, Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+
+        //--- по новому lambda выражение
+        map.forEach((key, value) -> System.out.println(key + " " + value));
+
+
+
+        //--------пример
+        Random random = new Random();
+        Map<Integer, Integer> map3 = new HashMap<>();
+
+        for (int i = 0; i < 100; i++) {
+            int num = random.nextInt(10);
+//            Integer val = map.get(num);
+//            if (val == null){
+//                map3.put(num, 1);
+//            } else{
+//                map3.put(num, val + 1);
+//            } начиная с 8й версии можно проще
+
+            map3.put(num, map3.getOrDefault(num, 0) + 1 ); //можно так 1 варик
+
+//            Integer val = map.getOrDefault(num, 0);//можно так 2 варик
+//            map3.put(num, val + 1);
+
+        }
+        System.out.println();
+        System.out.println(map3);
+
+        //----------------------------
+        //-------для map значением может быть другоя коллекция например List
+        Map<String, List<Integer>> mm = new HashMap<>();
+        mm.put("qsddfs", new ArrayList<>());
+
+
+
+
+
 
 
 
